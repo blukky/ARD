6df46b233e9a0fef359e18c2e5e5b899f6e4e22c
blukky <ya.gud@mail.ru>
2022-10-20 12:02:25 +0300

    ard-1

---
+++ ARD-1/arp-1.py
@@ -0,0 +1,40 @@
+class ARP_1:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def start(self, H):
+        pass
+
+    async def forward(self, speed):
+        pass
+
+    async def back(self, speed):
+        pass
+
+    async def left(self, speed):
+        pass
+
+    async def right(self, speed):
+        pass
+
+    async def rotation(self, angle):
+        pass
+
+    async def up(self, speed, distance):
+        pass
+
+    async def down(self, speed, distance):
+        pass
+
+    async def get_data_from_sensor(self):
+        pass
+
+    async def get_data_from_lidar(self):
+        pass
+
+    async def get_data_gyroscope(self):
+        pass
+
+    async def get_frame(self):
+        pass
